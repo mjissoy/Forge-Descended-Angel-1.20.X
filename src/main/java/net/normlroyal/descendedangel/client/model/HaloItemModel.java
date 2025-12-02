65535e92fce053a98e4dbb1b449cffadcf702b35
mjissoy <mjissoy@gmail.com>
2025-12-02 13:09:01 +1100

    Updated Geckolib rendering method

--- src/main/java/net/normlroyal/descendedangel/client/model/HaloItemModel.java
+++ src/main/java/net/normlroyal/descendedangel/client/model/HaloItemModel.java
@@ -1,24 +1,34 @@
 package net.normlroyal.descendedangel.client.model;
 
 import net.minecraft.resources.ResourceLocation;
+import net.normlroyal.descendedangel.DescendedAngel;
 import net.normlroyal.descendedangel.item.custom.TieredHaloItem;
 import software.bernie.geckolib.model.GeoModel;
 
 public class HaloItemModel extends GeoModel<TieredHaloItem> {
     @Override
     public ResourceLocation getModelResource(TieredHaloItem object) {
-        return new ResourceLocation("descendedangel", "geo/halo_t1.geo.json");
+        int tier = object.getTier(); // 1..9
+
+        return new ResourceLocation(
+                "descendedangel",
+                "geo/halo_t" + tier + ".geo.json"
+        );
     }
 
     @Override
     public ResourceLocation getTextureResource(TieredHaloItem object) {
-        return new ResourceLocation("descendedangel", "textures/item/halo.png");
-        // return new ResourceLocation("minecraft", "textures/item/apple.png");
+        int tier = object.getTier();
+
+        return new ResourceLocation(
+                "descendedangel",
+                "textures/item/halo_t" + tier + ".png"
+        );
     }
 
     @Override
     public ResourceLocation getAnimationResource(TieredHaloItem object) {
-        // if you don't have animations yet, you can temporarily return null
+        // later: "animations/halo_tX.animation.json" per tier if you want
         return null;
     }
 }
