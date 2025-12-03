839f1cf262508cdf2701623e9bd90d7c82e7723e
mjissoy <mjissoy@gmail.com>
2025-12-03 15:33:11 +1100

    Added JEI Compatabiliy and centered the item models when in the Inventory GUI. I also added basic recipes for the halos

--- src/main/java/net/normlroyal/descendedangel/client/render/HaloItemRenderer.java
+++ src/main/java/net/normlroyal/descendedangel/client/render/HaloItemRenderer.java
@@ -1,5 +1,9 @@
 package net.normlroyal.descendedangel.client.render;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
 import net.normlroyal.descendedangel.client.model.HaloItemModel;
 import net.normlroyal.descendedangel.item.custom.TieredHaloItem;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
@@ -8,4 +12,24 @@ public class HaloItemRenderer extends GeoItemRenderer<TieredHaloItem> {
     public HaloItemRenderer() {
         super(new HaloItemModel());
     }
+
+    @Override
+    public void renderByItem(ItemStack stack,
+                             ItemDisplayContext displayContext,
+                             PoseStack poseStack,
+                             MultiBufferSource buffer,
+                             int packedLight,
+                             int packedOverlay) {
+
+        poseStack.pushPose();
+
+        if (displayContext == ItemDisplayContext.GUI) {
+            poseStack.translate(0.05F, -0.4F, 0.0F);
+            poseStack.scale(0.9F, 0.9F, 0.9F);
+        }
+
+        super.renderByItem(stack, displayContext, poseStack, buffer, packedLight, packedOverlay);
+        poseStack.popPose();
+    }
+
 }
